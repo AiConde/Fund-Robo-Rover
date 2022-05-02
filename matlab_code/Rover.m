058bd37c15c1dd3b1565533b0826f01c0bcf7d73
Solomon Greenberg <solomongreenberg@gmail.com>
2022-05-02 12:25:54 -0400

    Fix merge issue

--- matlab_code/Rover.m
+++ matlab_code/Rover.m
@@ -5,13 +5,8 @@ classdef Rover < handle
         %% class properties
 
         % LOCALIZATION
-<<<<<<< HEAD
-        initial_robot_pose % [x, y, theta] 2D pose in world coordinates 
-        localiztion        % will return pose 2D + velcoities
-=======
         initial_robot_pose % [x, y, theta] 2D pose in world coordinates
         localization         % will return pose 2D + velcoities
->>>>>>> e2f05f205dd56c9f15d8107580ef307bda989827
         odometry           % odometer obj
         found_dock         % boolean of having located dock
         dock_pose          % dock pose in world coordinates
