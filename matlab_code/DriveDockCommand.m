8776fdfcf3446f4c065fc24231375096084af611
AiConde <48631164+AiConde@users.noreply.github.com>
2022-05-04 02:06:55 -0400

    Drive Dock Command Structure

---
+++ matlab_code/DriveDockCommand.m
@@ -0,0 +1,79 @@
+classdef DriveDockCommand  < Command
+
+
+    properties (Access = private)
+        waypoints;  % list of local wayponts
+        end_distance_tolerance;     
+        final_position;
+        dock_apriltag_distance;
+        
+
+        pp_controller;
+    end
+
+    methods
+        % waypoint_list is a list of global Pose2d waypoints
+        % end_dist_tolerance is the absolute distance tolerance (in meters) to the last waypoint, to determine if we're done with our path or not.
+        % linear_velocity is how fast we want to go, in m/s
+        % final_position is the final global position we want the rover to
+        % end up in
+        % dock_apriltag_distance is the distance between the rover and the
+        % dock's april tag
+
+        function obj = DriveDockCommand(rover_handle, DrivetrainController,waypoint_list, end_dist_tolerance,linear_velocity,final_position,dock_apriltag_distance)
+            obj@Command(rover_handle); % Pass rover_handle to the superclass Command constructor
+            obj.end_distance_tolerance = end_dist_tolerance;
+            obj.waypoints = waypoint_list;
+            obj.final_position = final_position;
+            obj.DrivetrainController = DrivetrainController();
+            obj.dock_apriltag_distance = dock_apriltag_distance;
+
+            waypoints_converted = zeros(size(waypoint_list,1), 2);
+            for i=1:size(waypoint_list,1)
+                waypoints_converted(:,i) = [waypoint_list(i).translation.val_x waypoint_list(i).translation.val_y];
+            end
+
+            obj.pp_controller = controllerPurePursuit( ...
+                'DesiredLinearVelocity',linear_velocity, ...
+                'LookaheadDistance', 1.0, ...
+                'MaxAngularVelocity', 1.0, ...
+                'Waypoints', waypoints_converted ...
+                );
+        end
+
+        function initialize(obj)
+            obj.dock_pose = obj.rover_handle.dock_pose.p2d.translation;
+            obj.
+
+
+        end
+
+        function execute(obj)
+            robot_pose = obj.rover_handle.localization.current_pose;
+            robot_pose_xyt = [robot_pose.translation.val_x robot_pose.translation.val_y robot_pose.rotation.value_radians];
+            [vel, ang_vel] = obj.pp_controller(robot_pose_xyt);
+            obj.rover_handle.drivetrain_controller.set_vel_setpoints(vel,ang_vel);
+           
+            if obj.dock_apriltag_distance > max_distance
+                
+            end
+        end
+
+        function done = is_done(obj)
+            is_timeout = obj.rover_handle.system_time > obj.start_time + obj.max_nav_time;
+
+            current_translation = obj.rover_handle.localization.current_pose.translation;
+            final_waypoint_translation = obj.waypoints(end).translation;
+            translation_delta = current_translation.get_distance(final_waypoint_translation);
+
+            is_path_complete = abs(translation_delta) < abs(obj.end_distance_tolerance);
+
+            done = is_timeout || is_path_complete;
+        end
+
+        function cmd_end(obj)
+            obj.rover_handle.drivetrain_controller.set_vel_setpoints(0,0);
+        end
+
+    end
+end
